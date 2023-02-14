6cc73e290e22f608971748c174edd88403a8d11b
Karina Ostrovska <karina.ostrovsika@gmail.com>
2023-02-14 16:37:20 +0200

    Reversed sequence

---
+++ 8-kyu/Reversed-sequence.js
@@ -0,0 +1,11 @@
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+const reverseSeq = n => {
+    const result = [];
+    for(let i = n; i > 0; i--){
+      result.push(i);
+    }
+    return result;
+  };
+    
+  
